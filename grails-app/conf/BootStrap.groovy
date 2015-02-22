6ea8badd516b0d6dc6c8b5eb2652408228b046f0
Esteban <iebvidea@gmail.com>
2015-02-22 16:05:29 -0500

    Creado usuario de prueba y carpetas de sprints

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -5,7 +5,7 @@ class BootStrap {
 
     def init = { servletContext ->
        new User(idUser:"123", userName:"123", password:"123").save()
-        
+       new User(idUser:"1111111111", userName:"Diez Unos", password:"123").save()
         
     }
     def destroy = {
