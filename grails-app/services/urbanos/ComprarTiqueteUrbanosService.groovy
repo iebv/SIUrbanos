acb2d280275294b948a1c3b7da554eeac546f17e
Esteban <iebvidea@gmail.com>
2015-06-15 10:53:35 -0500

    Corregidos problemas con el servicio

--- grails-app/services/urbanos/ComprarTiqueteUrbanosService.groovy
+++ grails-app/services/urbanos/ComprarTiqueteUrbanosService.groovy
@@ -7,11 +7,13 @@ import javax.jws.soap.*
 import javax.jws.*
 import javax.xml.ws.*
 import javax.xml.bind.annotation.*
+import javax.jws.soap.SOAPBinding.Style;
+import javax.jws.soap.SOAPBinding.ParameterStyle;
 
 @WebService (targetNamespace="http://SIUrbanos/")
-@SOAPBinding(parameterStyle=SOAPBinding.ParameterStyle.BARE)
+@SOAPBinding(style=Style.RPC, parameterStyle=ParameterStyle.WRAPPED)
 class ComprarTiqueteUrbanosService {
-
+    static expose = ['cxfjax']
     @WebMethod(operationName = "ExistenciaUsuario", action="http://SIUrbanos/ExistenciaUsuario")
     public boolean ExistenciaUsuario(@WebParam(name = "id") String id, @WebParam(name = "password") String password) {
         def user = User.find("from User as u where u.idUser = '${id}' and u.password = '${password}'")
@@ -24,16 +26,17 @@ class ComprarTiqueteUrbanosService {
     
     @WebMethod(operationName = "CrearUsuario", action="http://SIUrbanos/CrearUsuario")
     public boolean CrearUsuario (@WebParam(name = "id") String id, @WebParam(name = "password") String password){
-        
         def user = new User(idUser:id, userName:id, password:password, rol:"user")
-        agregarTarjeta(user)
-        agregarRecarga(user,200000)
-        agregarViaje(user,"k6")
-        
-        if(user) return true
-        else return false
+        if(user){
+            agregarTarjeta(user)
+            agregarRecarga(user,200000)
+            agregarViaje(user,"k6")
+            return true
+        } 
+        return false
     }
     
+    @WebMethod(operationName = "agregarTarjeta", action="http://SIUrbanos/agregarTarjeta")
     def agregarTarjeta(usuario){
         def idCard = usuario.idUser
         def card = new Card(idCard: idCard)
@@ -42,17 +45,19 @@ class ComprarTiqueteUrbanosService {
         usuario.save(flush:true)
     }
     
+    @WebMethod(operationName = "agregarRecarga", action="http://SIUrbanos/agregarRecarga")
     def agregarRecarga(usuario,monto){
         def card1 = usuario.card
         card1.recargar(monto)
         card1.save(flush:true)
     }
     
+    @WebMethod(operationName = "agregarViaje", action="http://SIUrbanos/agregarViaje")
     def agregarViaje(usuario,ruta){
         def card1 = usuario.card
         card1.viajar(ruta)
         card1.save(flush:true)
     }
     
-    
+   
 }
